03d3d300af50ae26fd46e0247222a0376dd730a5
klarissamm <klarissamunz@gmail.com>
2017-01-12 13:32:59 +0000

    Thurs lunch, client side jwts

--- vanc-app/server.js
+++ vanc-app/server.js
@@ -16,6 +16,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static(`${__dirname}/bower-components`));
 app.use(express.static(`${__dirname}/public`));
 
 app.use('/api', expressJWT({ secret: config.secret })

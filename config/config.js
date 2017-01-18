d80a668a591235303401af1c4528075279a4f2e6
klarissamm <klarissamunz@gmail.com>
2017-01-18 10:08:50 +0000

    added stuff to the config and border

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: 'mongodb://localhost/vanc-app',
-  secret: process.env.SECRET || 'wow this is secret.'
+  db: process.env.MONGODB_URI || 'mongodb://localhost/vanc-app',
+  secret: process.env.SECRET || 'secret.'
 };
